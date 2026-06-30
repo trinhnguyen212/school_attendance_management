302622265cea5487450bd2ebb15de8217205a009
trinhnguyen212 <tngu212@mywhitecliffe.com>
2026-06-30 14:57:55 +1000

    token fix

--- backend/app/Models/PersonalAccessToken.php
+++ backend/app/Models/PersonalAccessToken.php
@@ -6,17 +6,17 @@ use App\Observers\PersonalAccessTokenObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-#[ObservedBy(PersonalAccessTokenObserver::class)]
+// #[ObservedBy(PersonalAccessTokenObserver::class)]
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
-    // protected $fillable = [
-    //     'name',
-    //     'ip',
-    //     'user_agent',
-    //     'token',
-    //     'abilities',
-    //     'expires_at',
-    // ];
+    protected $fillable = [
+        'name',
+        'ip',
+        'user_agent',
+        'token',
+        'abilities',
+        'expires_at',
+    ];
 
     protected $hidden = [
         'token',
